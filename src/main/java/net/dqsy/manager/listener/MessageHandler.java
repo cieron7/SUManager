922ae56f729c1dc3a9ce79c150a931bef32bc9bd
zjing <zjing@ablesky.com>
2017-03-23 10:52:12 +0800

    add listener

---
+++ src/main/java/net/dqsy/manager/listener/MessageHandler.java
@@ -0,0 +1,20 @@
+package net.dqsy.manager.listener;
+
+import net.dqsy.manager.controller.LoginController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageHandler implements ApplicationListener<ContextRefreshedEvent>{
+    public static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        logger.info("---------------init-------------------");
+
+    }
+}
