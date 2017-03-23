373c4549017c1ac364d39febbe3d068780982f0b
Brobao <zjing@ablesky.com>
2017-03-23 10:58:34 +0800

    blank commit

--- src/main/java/net/dqsy/manager/listener/MessageHandler.java
+++ src/main/java/net/dqsy/manager/listener/MessageHandler.java
@@ -16,5 +16,6 @@ public class MessageHandler implements ApplicationListener<ContextRefreshedEvent
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         logger.info("---------------init-------------------");
 
+
     }
 }
