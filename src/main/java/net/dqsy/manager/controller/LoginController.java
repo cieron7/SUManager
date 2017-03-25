ab282b24bab5b31dfbb866146665f996d8e7abe0
Brobao <jingzhibao1993@163.com>
2017-03-25 23:01:40 +0800

    user login method, todo about redis

--- src/main/java/net/dqsy/manager/controller/LoginController.java
+++ src/main/java/net/dqsy/manager/controller/LoginController.java
@@ -2,6 +2,8 @@ package net.dqsy.manager.controller;
 
 import net.dqsy.manager.pojo.Account;
 import net.dqsy.manager.service.AccountService;
+import net.dqsy.manager.web.util.IpUtil;
+import net.dqsy.manager.web.util.MD5Util;
 import net.dqsy.manager.web.util.ParamUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,31 +27,38 @@ public class LoginController {
 
     @Autowired
     private AccountService accountService;
+
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         // TODO: 2017/3/25 判断是否登陆 可以在拦截器中配置
-        if(true){
+        if (true) {
             return "redirect:login.html";
-        }else{
+        } else {
             return "redirect:index.html";
         }
     }
 
 
     //登录功能
-    @RequestMapping("/logon")
-    public String logon(HttpServletRequest request, HttpServletResponse response , Model model){
+    @RequestMapping("/signin")
+    public String logon(HttpServletRequest request, HttpServletResponse response, Model model) {
         //获取前台界面的username和password
         String username = ParamUtils.getParameter(request, "username", "");
         String password = ParamUtils.getParameter(request, "password", "");
+
+        password = MD5Util.getMD5(password.getBytes());
         Account account = accountService.logon(username.trim(), password);
-        if(account == null) {
-            model.addAttribute(username);
-            model.addAttribute("msg","用户名或密码错误");
-            return "forward:/login";
+        if (account == null) {
+            //model.addAttribute(username);
+            //model.addAttribute("msg","用户名或密码错误");
+            //return "forward:/login";
+            return "redirect:login.html";
         }
         //将用户护具存入session
-        request.getSession().setAttribute("username",username);
-        return "page/manager";
+        //request.getSession().setAttribute("username",username);
+        String ip = IpUtil.getIpStr(request);
+        String uri = request.getRequestURI();
+        logger.info("user: {}, ip: {}", account.getUsername(), ip);
+        return "redirect:index.html";
     }
 }
