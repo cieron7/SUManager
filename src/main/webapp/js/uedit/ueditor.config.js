5d1569a97f33bbe84d4f1db6aa85a06dac88fe3d
Brobao <jingzhibao1993@163.com>
2017-04-05 01:04:30 +0800

    update index.html

--- src/main/webapp/js/uedit/ueditor.config.js
+++ src/main/webapp/js/uedit/ueditor.config.js
@@ -42,7 +42,7 @@
             'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
             'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter',
 			'|','simpleupload', 'insertimage', 'emotion', 'insertvideo', 'music', 'attachment', 'map', 'insertframe', 'insertcode', 'pagebreak', 'template', 'background',
-			'|', 'horizontal', 'date', 'time', 'spechars'
+			'|', 'horizontal', 'date', 'time', 'fullscreen'
         ]]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
